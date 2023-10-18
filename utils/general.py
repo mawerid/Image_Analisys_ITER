25ec5d5642a85c49edacca38ff3702dd65f8bafb
Max Mawerid <monasmax02@gmail.com>
2023-10-18 19:33:33 +0300

    Hotfix 2

--- utils/general.py
+++ utils/general.py
@@ -654,7 +654,7 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
     # Settings
     # (pixels) minimum and maximum box width and height
     min_wh, max_wh = 2, 4096
-    max_det = 300  # maximum number of detections per image
+    max_det = 2000  # maximum number of detections per image
     max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
     time_limit = 10.0  # seconds to quit after
     redundant = True  # require redundant detections
